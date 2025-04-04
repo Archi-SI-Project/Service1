45b9bee2aec0f48ed846ec723e19e488a5cbe05d
Max-Tonnadre <max.tonnadre@efrei.net>
2025-04-04 16:34:22 +0200

    init

--- Service1/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Service1/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.service1;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Service1ApplicationTests {
+class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
